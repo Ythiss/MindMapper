c7d273d955fb7de800c54dd32dd4ceed997fa6a9
Ythiss <amaia.hiribarne@gmail.com>
2019-07-06 17:21:26 +0200

    [TASK] add new component for header html tag

---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Header() {
+    return (
+        <header className="App-header">
+            <h1>MindMapper</h1>
+        </header>
+    );
+  }
+  
+  export default Header;
